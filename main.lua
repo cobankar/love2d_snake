ff4d7a4aeeb67c2fd2da3854540c9ab0e7d736a3
Luca Huber <luca@belegerfassung.ch>
2023-04-07 19:12:57 +0200

    remove unused constants

--- main.lua
+++ main.lua
@@ -1,22 +1,13 @@
-
-
-local const = {}
-const.pixel = 16
-
-
--- Load some default values for our rectangle.
 function love.load()
   require("snake")
 
   Snake:load()
 end
 
--- Increase the size of the rectangle every frame.
 function love.update(dt)
   Snake:update(dt)
 end
 
--- Draw a coloured rectangle.
 function love.draw()
   Snake:draw()
 end
